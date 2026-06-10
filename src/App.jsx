331e9840c8751c8a5b55fa74608f83daf52e00c3
sonalbhayani <bhayanisonal84@gmail.com>
2026-06-10 20:04:11 +0530

    project done

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,116 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+
+import {useState,useEffect} from "react";
+import axios from "axios";
+import {Box,InputLabel,MenuItem,FormControl,Select} from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [countries,setCountries]=useState("");
+  const [country,setCountry]=useState("");
+  const [states,setStates]=useState("");
+  const [state,setState]=useState("");
+  const [cities,setCities]=useState("");
+  const [city,setCity]=useState("");
+  function getStates(country){
+    axios.get(`https://location-selector.labs.crio.do/country=${country}/states`).then((res)=>{
+      setStates(res.data);
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
+  function getCities  (country,state){
+    axios.get(`https://location-selector.labs.crio.do/country=${country}/state=${state}/cities`).then((res)=>{
+      setCities(res.data);
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
+  useEffect(()=>{
+    
+    axios.get("https://location-selector.labs.crio.do/countries").then((res)=>{
+     
+      setCountries(res.data);
+    }).catch((err)=>{
+      console.log(err);
+    })
+  },[])
+  const handleChange=(event,field)=>{
+    if(field==='country'){
+      setCountry(event.target.value);
+      getStates(event.target.value);
+    }else if(field==='state'){
+      setState(event.target.value);
+      getCities(country,event.target.value);
+    }else if(field==='city'){
+      setCity(event.target.value);
+    }
+  }
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
+   
+      <Box sx={{ minWidth: 120 }}>
+       <FormControl variant="outlined" sx={{ m: 1, minWidth: 300 }}>
+        <InputLabel id="select-outlined-label-country">Select Country</InputLabel>
+        <Select
+          labelId="select-outlined-label-country"
+          id="select-outlined-country"
+          value={country}
+          onChange={(event) => handleChange(event,'country') }
+          label="Age"
         >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+          {countries && countries.map((country)=>{
+            return <MenuItem  key={country} value={country}>{country}</MenuItem>
+          })}
+        
+        </Select>
+      </FormControl>
+      <FormControl variant="outlined" sx={{ m: 1, minWidth: 300 }}>
+        <InputLabel id="select-outlined-label-state">Select State</InputLabel>
+        <Select
+          labelId="select-outlined-label-state"
+          id="select-outlined-state"
+          disabled={country?false:true}
+          value={state}
+          onChange={(event) => handleChange(event, 'state') }
+        >
+          {!states && (
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+          )}
+          {states && states.map((state)=>{
+            console.log(state);
+            return <MenuItem  key={state} value={state}>{state}</MenuItem>
+          })}
+        </Select>
+      </FormControl>
+      <FormControl variant="outlined" sx={{ m: 1, minWidth: 300 }}>
+        <InputLabel id="select-outlined-label-city">Select City</InputLabel>
+        <Select
+          labelId="select-outlined-label-city"
+          id="select-outlined-city"
+          disabled={state?false:true}
+          value={city}
+          onChange={(event) => handleChange(event, 'city') }
+         >  
+        
+          {!cities && (
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+          )}
+          {cities && cities.map((city)=>{
+            console.log(city);
+            return <MenuItem  key={city} value={city}>{city}</MenuItem>
+          })}
+        </Select>
+      </FormControl>
+      {country && state && city && (
+        <Box sx={{ m: 1, minWidth: 300 }}>
+          <p> <strong> You Selected {city},</strong>
+          {state}, {country}</p>
+        </Box>
+      )}
+    </Box>
+    
   )
 }
 
